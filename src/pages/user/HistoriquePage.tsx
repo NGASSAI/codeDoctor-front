ea6f6c4fdf8a33e060bcc0dc3a08dc6581ebe8da
NGASSAI <comptengassai@gmail.com>
2026-09-02 10:35:17 +0100

    correction de couleur historique

--- src/pages/user/HistoriquePage.tsx
+++ src/pages/user/HistoriquePage.tsx
@@ -424,7 +424,7 @@ export default function HistoriquePage() {
                       rounded-xl bg-blue-500
                       px-4 py-2.5
                       text-sm font-medium text-white
-                      transition hover:bg-zinc-800
+                      transition hover:bg-green-600
                     "
                   >
                     Voir le détail
